8fc0af504d460c110930590e2413aeaf03f96fa1
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-13 09:58:16 +0100

    lösung Aufgabe9

--- test/PointTest.java
+++ test/PointTest.java
@@ -1,44 +1,50 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointTest {
 
+    private static final double DELTA= 0.001;
+
+    private Point pointOne;
+    private Point pointTwo;
+
+    @BeforeEach
+    public void initPoints() {
+        pointOne = new Point(10.0, 10.0);
+        pointTwo = new Point(2.0, 2.0);
+    }
+
     @Test
     void add() {
-        Point testPoint1 = new Point(1.0, 1.0);
-        Point testPoint2 = new Point(3.0, 1.0);
-        Point testPoint3 = testPoint1.add(testPoint2);
+        Point pointThree = pointOne.add(pointTwo);
 
-        assertEquals(testPoint3.getX(), 4, 0.01);
-        assertEquals(testPoint3.getY(), 2, 0.01);
+        assertEquals(pointThree.getX(), 12.0, DELTA);
+        assertEquals(pointThree.getY(), 12.0, DELTA);
     }
 
     @Test
     void sub() {
-        Point testPoint1 = new Point(1.0, 1.0);
-        Point testPoint2 = new Point(3.0, 1.0);
-        Point testPoint3 = testPoint1.sub(testPoint2);
+        Point pointThree = pointOne.sub(pointTwo);
 
-        assertEquals(testPoint3.getX(), -2, 0.01);
-        assertEquals(testPoint3.getY(), 0, 0.01);
+        assertEquals(pointThree.getX(), 8.0, DELTA);
+        assertEquals(pointThree.getY(), 8.0, DELTA);
     }
 
     @Test
     void mult() {
-        Point testPoint1 = new Point(1.0, 1.0);
-        Point testPoint3 = testPoint1.mult(3.0);
+        Point pointThree = pointOne.mult(2.0);
 
-        assertEquals(testPoint3.getX(), 3, 0.01);
-        assertEquals(testPoint3.getY(), 3, 0.01);
+        assertEquals(pointThree.getX(), 20.0, DELTA);
+        assertEquals(pointThree.getY(), 20.0, DELTA);
     }
 
     @Test
     void div() {
-        Point testPoint1 = new Point(10.0, 10.0);
-        Point testPoint3 = testPoint1.div(2.0);
+        Point pointThree = pointOne.div(2.0);
 
-        assertEquals(testPoint3.getX(), 5.0, 0.01);
-        assertEquals(testPoint3.getY(), 5.0, 0.01);
+        assertEquals(pointThree.getX(), 5.0, DELTA);
+        assertEquals(pointThree.getY(), 5.0, DELTA);
     }
 }
