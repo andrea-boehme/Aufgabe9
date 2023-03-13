6d968530d6a8362d537537030c6acdf5b5fd8b66
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-13 09:02:28 +0100

    lösung Aufgabe9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Point p1 = new Point(1,1);
+        Point p2 = new Point(3,1);
+        Point p3 = p1.add(p2);
+        Point p4 = p1.sub(p2);
+        Point p5 = p1.mult(p2);
+        Point p6 = p1.div(p2);
+
+        System.out.println( "Point 1: " + p1 + ", Point 2: " + p2);
+        System.out.println("Addition: " + p3);
+        System.out.println("Substraction: " + p4);
+        System.out.println("Multiplication: " + p5);
+        System.out.println("Division: " + p6);
+
     }
 }
---
+++ src/Point.java
@@ -0,0 +1,57 @@
+public class Point {
+    // define attributes (two dimensions)
+    private double x;
+    private double y;
+
+    // create constructor method to define a point object at a specific x,y location:
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // create getter method for x
+    public double getX() {
+        return x;
+    };
+
+    // create getter method for y
+    public double getY() {
+        return y;
+    };
+
+    public Point add(Point p) {
+        double x_result = x + p.getX();
+        double y_result = y + p.getY();
+
+        return new Point(x_result, y_result);
+    }
+
+    public Point sub(Point p) {
+        double x_result = x - p.getX();
+        double y_result = y - p.getY();
+
+        return new Point(x_result, y_result);
+    }
+
+    public Point mult(Point p) {
+        double x_result = x * p.getX();
+        double y_result = y * p.getY();
+
+        return new Point(x_result, y_result);
+    }
+
+    public Point div(Point p) {
+        double x_result = x / p.getX();
+        double y_result = y / p.getY();
+
+        return new Point(x_result, y_result);
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
